56b894d84d2b44410faa10076e079a4ea7e27f9e
Damien LAURENS <damienlaurens64@gmail.com>
2017-11-28 17:31:31 +0100

    MAJ

--- app-pokemon/src/app/app.module.ts
+++ app-pokemon/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { ShadowCardDirective } from './shadow-card.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShadowCardDirective
   ],
   imports: [
     BrowserModule
--- app-pokemon/src/app/shadow-card.directive.ts
+++ app-pokemon/src/app/shadow-card.directive.ts
@@ -1,19 +1,28 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener } from '@angular/core';
 
-@Directive({ selector: '[pkmn-shadow-card]' })
+@Directive({ selector: '[appShadowCard]' })
 export class ShadowCardDirective {
-    constructor(el: ElementRef, renderer: Renderer) {
+
+    constructor(private el: ElementRef, private renderer: Renderer) {
        this.setBorder('#f5f5f5');
-       this.setHeight('180px');
+//       this.setHeight('180px');
     }
 
     private setBorder(color: string) {
-        let style = 'solid 4px ' + color;
+        const style = 'solid 1px ' + color;
         this.renderer.setElementStyle(this.el.nativeElement, 'border', style);
     }
 
     private setHeight(height: string) {
         this.renderer.setElementStyle(this.el.nativeElement, 'height', height);
+    }
+
+    @HostListener('mouseenter') onMouseEnter() {
+        this.setBorder('rgba(0, 0, 0, 0.125)');
+    }
+
+    @HostListener('mouseleave') onMouseLeave() {
+        this.setBorder('rgba(0, 0, 0, 0.125)');
     }
 
 }
