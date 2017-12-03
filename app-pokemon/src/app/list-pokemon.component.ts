4b9140461963edcc599698b665b4c1158f83e4c3
LAURENS Damien <damienlaurens64@gmail.com>
2017-12-03 21:42:11 +0100

    MAJ

---
+++ app-pokemon/src/app/list-pokemon.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { POKEMONS } from './mock-pokemons';
+import { Pokemon } from './pokemon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class ListPokemonComponent {
+  pokemons = POKEMONS;
+  values = '';
+  values2 = '';
+  values3 = '';
+
+
+  constructor(private router: Router) {
+
+  }
+
+  selectPokemon(pokemon: Pokemon): void {
+    console.log('Vous avez selectionné ' + pokemon.name);
+    const link = ['/pokemon', pokemon.id];
+    this.router.navigate(link);
+}
+
+  onKey(event: KeyboardEvent) {
+        this.values = 'Bonjour ' + (<HTMLInputElement>event.target).value; // texte entré par l'utilisateur
+  }
+
+  onKey2(value: string) {
+    this.values2 = 'Bonjour ' + value;
+  }
+
+  onKey3(value: string) {
+    this.values3 = 'Bonjour ' + value;
+  }
+}
