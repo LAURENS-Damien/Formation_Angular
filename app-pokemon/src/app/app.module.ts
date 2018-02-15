35369f53e858d22c7123ec25a6bf93c4c55473ee
LAURENS Damien <damienlaurens64@gmail.com>
2018-02-15 21:22:12 +0100

    MAJ

--- app-pokemon/src/app/app.module.ts
+++ app-pokemon/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginRoutingModule } from './pokemons/login-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,7 @@ import { PokemonsModule } from './pokemons/pokemons.module';
     LoginRoutingModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
